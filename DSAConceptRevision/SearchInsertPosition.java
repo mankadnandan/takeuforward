7d6485106f4b61e79c51b694542a97d80e95f9ac
Nandan Mankad <mankadnandan@gmail.com>
2025-06-10 13:56:15 +0530

    Commiting SearchInsertPosition

---
+++ DSAConceptRevision/SearchInsertPosition.java
@@ -0,0 +1,22 @@
+public class SearchInsertPosition {
+    public static void main(String[] args) {
+        System.out.println(searchInsert(new int[] { 1, 3, 5, 6 }, 7));
+    }
+
+    public static int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int mid = 0;
+        while (left <= right) {
+            mid = left + ((right - left) / 2);
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return nums[mid] > target ? mid : mid + 1;
+    }
+}
